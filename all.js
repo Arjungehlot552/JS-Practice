e9f4fb1a3caaded746e257b543ba4127de4853ac
Arjun <arjungehlot552@gmail.com>
2024-01-04 16:14:42 +0530

    My today practice commit

---
+++ all.js
@@ -0,0 +1,13 @@
+var x ,y;
+x = 2+5+3;
+y = x  * 5;
+document.getElementById('demo').innerHTML=y;
+/*
+hello i am arjun gehlot from ies college of technology bhopal and i am here to present my coding journey to you and nothing to say
+*/
+
+const pi = 3.14;
+const FirstName = "Arjun";
+const LastName = "Gehlot";
+
+document.getElementById('demo2').innerHTML = pi + "<br>" +FirstName+ "<br>" +LastName;
