18d9e9eba05acadbf5d505a72e9f889668dfb95f
Arjun <arjungehlot552@gmail.com>
2024-03-05 18:19:06 +0530

    my new commit

--- function.js
+++ function.js
@@ -1,6 +1,70 @@
-function average(a ,b, c) {
-    let average = (a+b+c) / 3;
-   console.log(average);
+// function average(a ,b, c) {
+//     let average = (a+b+c) / 3;
+//    console.log(average);
+// }
+
+// average(1, 2, 3);
+
+
+// function myMap(arr, callback) {
+//     let result = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         result.push(callback(arr[i], i, arr));
+//     }
+//     return result;
+// }
+
+
+// let object = {
+//     {
+//         name  : "Arjun",
+//         age : 20,
+//     },
+//     {
+//         name  : "Karan",
+//         age : 25,
+//     },
+// };
+
+// console.log(object.name);
+
+//   let time = setTimeout(() => {
+//   console.log("This is my setTime Out function :")
+// } , 2000);
+
+// try {
+//     console.log(a);
+// } 
+// catch (error) {
+//     console.log("a is not defined please difine first" , error)
+// }
+let myarr = {}; // Use an object instead of an array
+
+let keys = ['newKey1', 'newKey2', 'newKey3'];
+let values = ['newValue1', 'newValue2', 'newValue3'];
+
+for (let i = 0; i < keys.length; i++) {
+    myarr[keys[i]] = values[i]; // Assign key-value pairs to the object
 }
 
-average(1, 2, 3);
+console.log(myarr);
+
+function sum(args)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
